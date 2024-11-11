f5e8162ebb2e99a1679956de71bc4cdae348f559
LepharamRamchiary <lepharamchiary@gmail.com>
2024-11-11 08:21:42 +0530

    users account activation

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -11,7 +11,7 @@ class Register  extends BaseController
     public $email;
     public function __construct()
     {
-        helper('form');
+        helper(['form', 'date']);
         $this->registerModel = new RegisterModel();
         $this->session = \Config\Services::session();
         $this->email = \Config\Services::email();
@@ -43,7 +43,7 @@ class Register  extends BaseController
                 if ($this->registerModel->createUser($userdata)) {
                     $to = $this->request->getVar('email');
                     $subject = 'Account Activation - FinCo';
-                    $message = 'Hi ' . $this->request->getVar('username', FILTER_SANITIZE_STRING) . ',<br><br>Thanks your account create successfully' . '<br><br>Please click the link below to activate your account.<br><br>' . '<a href="' . base_url() . '/register/activate/' . $uniid . '" target="_blank">Activate Now</a>';
+                    $message = 'Hi ' . $this->request->getVar('username', FILTER_SANITIZE_STRING) . ',<br><br>Thanks your account create successfully' . '<br><br>Please click the link below to activate your account.<br><br>' . '<a href="' . base_url() . 'register/activate/' . $uniid . '" target="_blank">Activate Now</a>';
 
                     $this->email->setTo($to);
                     $this->email->setFrom('lepharamchiary123@gmail.com', 'Info');
@@ -51,7 +51,7 @@ class Register  extends BaseController
                     $this->email->setMessage($message);
                     if ($this->email->send()) {
                         $this->session->setTempdata('success', 'Account Created Successfully. Please check your email for activation link.', 3);
-                    return redirect()->to(current_url());
+                        return redirect()->to(current_url());
                     } else {
                         $this->session->setTempdata('error', 'Account Created Successfully. Unable to send activation link. Contact Admin', 3);
                         return redirect()->to(current_url());
@@ -66,5 +66,52 @@ class Register  extends BaseController
         }
 
         return view('register_view', $data);
+    }
+
+    public function activate($uniid = null)
+    {
+
+        $data = [];
+        if (!empty($uniid)) {
+            $userdata = $this->registerModel->verifyUniid($uniid);
+            // print_r($data);
+            // exit;
+
+            if ($userdata) {
+                if ($this->verifyExpiryTime($userdata->activation_date)) {
+                   if($userdata->status == 'inactive'){
+                      $status = $this->registerModel->updateStatus($uniid);
+                      if($status){
+                          $data['success'] = 'Your account is actived successfully.';
+                      }else{
+                          $data['error'] = 'Sorry! Unable to activate your account.';
+                      }
+                   }else{
+                    $data['success'] = 'Your account is already actived.';
+                   }
+                } else {
+                    $data['error'] = 'Sorry! Activation link was expired.';
+                }
+            } else {
+                $data['error'] = 'Sorry! We are unable to find your account.';
+            }
+        } else {
+            $data['error'] = 'Sorry! Unable to activate your account.';
+        }
+
+        return view('activate_view', $data);
+    }
+
+    public function verifyExpiryTime($resTime)
+    {
+        $currTime = now();
+        $regtime = strtotime($resTime);
+        $diffTime = $currTime - $regtime;
+
+        if (3600 > $diffTime) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
---
+++ app/Views/activate_view.php
@@ -0,0 +1,22 @@
+<?= $this->extend('layouts/base');?>
+
+<?= $this->section('content');?>
+
+
+<div class="container">
+    <h1>Account Process</h1>
+    <?php if (isset($error)): ?>
+        <div class="alert alert-danger">
+            <?= $error; ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (isset($success)): ?>
+        <div class="alert alert-success">
+            <?= $success; ?>
+        </div>
+    <?php endif; ?>
+</div>
+
+
+<?= $this->endSection();?>
